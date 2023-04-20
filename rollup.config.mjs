d40fda03e780d8f7d5cc50516ac7466282a6d5dd
Phương Bách <93362476+beebeo@users.noreply.github.com>
2023-04-20 15:57:36 +0700

    Initial commit

---
+++ rollup.config.mjs
@@ -0,0 +1,70 @@
+import json from '@rollup/plugin-json'
+import vuePlugin from 'rollup-plugin-vue'
+import { chromeExtension, simpleReloader } from 'rollup-plugin-chrome-extension'
+import { emptyDir } from 'rollup-plugin-empty-dir'
+import typescript from 'rollup-plugin-typescript2' // '@rollup/plugin-typescript'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import replace from '@rollup/plugin-replace'
+import postcss from 'rollup-plugin-postcss'
+import alias from 'rollup-plugin-alias'
+import _dotenv from 'dotenv/config'
+import url from 'rollup-plugin-url'
+import path from 'path'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default {
+    input: 'src/manifest.json',
+    output: {
+        dir: 'dist',
+        format: 'esm',
+        chunkFileNames: '[name]-[hash].js'
+    },
+    onwarn: (warning, defaultHandler) => {
+        if (warning.code === 'THIS_IS_UNDEFINED') return
+        defaultHandler(warning)
+    },
+    watch: { clearScreen: true }, // for dev debug
+    plugins: [
+        alias({
+            entries: {
+                ['@']: path.resolve(__dirname, 'src')
+            }
+        }),
+        // chromeExtension() must be first, in order to properly treat manifest.json as the entry point
+        chromeExtension({
+            extendManifest: {
+                key: process.env.VUE_APP_MV3_KEY
+            }
+        }),
+        // simpleReloader(), // Adds a Chrome extension reloader during watch mode
+        vuePlugin({ target: 'browser' }),
+        replace({
+            __VUE_OPTIONS_API__: true,
+            __VUE_PROD_DEVTOOLS__: false,
+            preventAssignment: true,
+            'process.env.NODE_ENV': JSON.stringify('production'),
+            'process.env.VUE_APP_FIREBASE_APIKEY': JSON.stringify(process.env.VUE_APP_FIREBASE_APIKEY),
+            'process.env.VUE_APP_FIREBASE_AUTHDOMAIN': JSON.stringify(process.env.VUE_APP_FIREBASE_AUTHDOMAIN),
+            'process.env.VUE_APP_FIREBASE_PROJECTID': JSON.stringify(process.env.VUE_APP_FIREBASE_PROJECTID),
+            'process.env.VUE_APP_FIREBASE_STORAGEBUCKET': JSON.stringify(process.env.VUE_APP_FIREBASE_STORAGEBUCKET),
+            'process.env.VUE_APP_FIREBASE_MESSAGINGSENDERID': JSON.stringify(process.env.VUE_APP_FIREBASE_MESSAGINGSENDERID),
+            'process.env.VUE_APP_FIREBASE_APPID': JSON.stringify(process.env.VUE_APP_FIREBASE_APPID),
+            'process.env.VUE_APP_MEASUREMENTID': JSON.stringify(process.env.VUE_APP_MEASUREMENTID)
+        }),
+        typescript(),
+        postcss(),
+        json(),
+        resolve(),
+        commonjs(),
+        emptyDir(),
+        url({
+            include: ['**/*.svg', '**/*.png', '**/*.jpg', '**/*.gif', '**/*.woff', '**/*.woff2'],
+            limit: Infinity,
+            publicPath: '/dist'
+        })
+    ]
+}
---
+++ src/popup/Popup.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        1234567890123
+
+        <div class="text">
+            123
+
+            <div class="text-2">bbbbb1</div>
+
+            <img src="./../assets/vue.svg" alt="" />
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.text {
+    color: red;
+
+    .text-2 {
+        color: blue;
+    }
+}
+</style>
+
+<script>
+export default {}
+</script>
